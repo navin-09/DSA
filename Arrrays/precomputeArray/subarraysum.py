a23af7865d58adc1921b5f7948504c7ce5fd5abe
Sandula Naveen <sandula.navin@gmail.com>
2025-10-27 17:10:38 +0530

    update subarry

--- Arrrays/precomputeArray/subarraysum.py
+++ Arrrays/precomputeArray/subarraysum.py
@@ -14,8 +14,20 @@ class Subarray:
             max_sum = max(max_sum,curr_sum)
 
         return max_sum
+    
+    def max_subarray_sum_with_len_k_sliding_window(self,arr,k):
+        sumk = sum(arr[:k])
+        max_sum = 0
+        j=0
+        for i in range(k,len(arr)):
+            sumk = sumk - arr[j] + arr[i] 
+            max_sum = max(max_sum,sumk)
+            j+=1
+        return max_sum
+
 
 subarray = Subarray()
 total_subarray_sum = subarray.total_subarray_sum_contribution_method([1,2,3])
 max_subarray_sum = subarray.max_subarray_sum_kadens([-1,2,-3,4,-5,6])
-print({'total_subarray_sum':total_subarray_sum,'max_subarray_sum':max_subarray_sum})
+max_subarray_sum_len_k = subarray.max_subarray_sum_with_len_k_sliding_window([-1,2,-3,4,-5,6],3)
+print({'total_subarray_sum':total_subarray_sum,'max_subarray_sum':max_subarray_sum,'max_subarray_sum_len_k':max_subarray_sum_len_k})
