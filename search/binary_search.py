163be6f7031795d0f90661ad05bffd6c914087cd
Sandula Naveen <sandula.navin@gmail.com>
2025-10-28 17:50:50 +0530

    binary search

--- search/binary_search.py
+++ search/binary_search.py
@@ -1,27 +1,21 @@
-import random
-def quick_sort(nums):
-    if len(nums) <=1:return nums
-    pivot = random.choice(nums)
-    left = [n for n in nums if n < pivot]
-    mid = [n for n in nums if n == pivot]
-    right = [n for n in nums if n > pivot]
+def binary_search(arr, target):
+    arr.sort()  # ensure array is sorted
+    print("Sorted:", arr)
 
-    return quick_sort(left) + mid + quick_sort(right)
+    left, right = 0, len(arr) - 1
 
-def binary_search(nums, n):
-    sort_nums = quick_sort(nums)
-    print(sort_nums)
-    left = 0
-    right = len(nums) - 1
-    while(left < right):
-        mid = (left+right)//2
-        if nums[mid] == n : return True
-        if nums[mid] < n : left = mid
-        if nums[mid] > n : right = mid
-    
-    return False
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return True, mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
 
-nums = [9, 3, 5, 7, 6, 2, 1, 9, 0,-1]
+    return False, -1
 
+
+# Example
+nums = [9, 3, 5, 7, 6, 2, 1, 9, 0, -1]
 print(binary_search(nums, 5))
-
