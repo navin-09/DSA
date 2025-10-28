37a15c56ea3a088382bacf7239e1c9bf630ff64b
Sandula Naveen <sandula.navin@gmail.com>
2025-10-28 18:06:58 +0530

    find peak

---
+++ search/find_peak.py
@@ -0,0 +1,12 @@
+def find_peak(nums):
+    left = 0
+    right = len(nums) - 1 
+    while left < right:
+        mid = (left+right)//2
+        if nums[mid] < nums[mid+1]:
+            left = mid+1
+        else:
+            right = mid
+    return left, nums[left]
+
+print(find_peak([1, 2, 3, 4, 5, 6, 5, 4, 3, 2, 1]))
